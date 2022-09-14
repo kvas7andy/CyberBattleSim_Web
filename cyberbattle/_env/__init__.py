973ace05c17eb8e2386b5852469ab3bbbf478a86
Andrei Kvasov <andrei.kvasov.dl@gmail.com>
2022-09-14 08:32:18 +0000

    tiny changes

---
+++ cyberbattle/_env/__init__.py
@@ -0,0 +1,9 @@
+# TODO check if that is necessary for registration
+# import cyberbattle is mostly enough
+
+
+# from cyberbattle._env.cyberbattle_toyctf import CyberBattleToyCtf
+# from cyberbattle._env.cyberbattle_toyctf import CyberBattleToyCtf
+# from cyberbattle._env.cyberbattle_toyctf import CyberBattleToyCtf
+# from cyberbattle._env.cyberbattle_toyctf import CyberBattleToyCtf
+# from cyberbattle._env.cyberbattle_toyctf import CyberBattleToyCtf
