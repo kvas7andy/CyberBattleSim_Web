09b307ae1069bb96f665a470e00de991e48550b4
Andrei Kvasov <andrei.kvasov.dl@gmail.com>
2022-09-27 13:45:55 +0000

    jupyter extensions, plotting figsize and benchmark.ipynb

--- install-pythonpackages.sh
+++ install-pythonpackages.sh
@@ -12,6 +12,7 @@ $PYTHON -m pip install --upgrade pip
 $PYTHON -m pip install wheel
 $PYTHON -m pip install -e .
 $PYTHON -m pip install -e .[dev]
+$PYTHON -m pip install jupyter_contrib_nbextensions && jupyter contrib nbextension install 
 
 if [ ""$GITHUB_ACTION"" == "" ]; then
   # Only install the `pre-commit` package
