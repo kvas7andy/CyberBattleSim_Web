6918ddc169abbb7d5b9a67b094e89b48a19b46bb
Andrei Kvasov <andrei.kvasov.dl@gmail.com>
2023-03-05 12:38:49 +0000

    1) Update the README.md with enabling running docker with GPUs 2) reafactor .sh code for experiments, more clear and easy to handle code 3) Deception evaluation .ipynb updated with violin and bar plots of detection points

--- run_exper_ht_gradual_increase.sh
+++ run_exper_ht_gradual_increase.sh
@@ -9,18 +9,14 @@ OUTPUT_DIR=notebooks  #../../../../
 INPUT_DIR=cyberbattle/agents/baseline/notebooks
 REWARD_CLIP=True
 
-# cat $INPUT_DIR/notebook_debug_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_tiny.ipynb -p gymid 'CyberBattleTinyMicro-v1' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # -p iteration_count 20 -p eval_episode_count 10
+# cat $INPUT_DIR/notebook_debug_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_tiny.ipynb -p gymid 'CyberBattleTinyMicro-v1' -p training_episode_count  2000 $arg # -p iteration_count 20 -p eval_episode_count 10
 
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v12' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # -p iteration_count 20 -p eval_episode_count 10
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v123' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # -p iteration_count 20 -p eval_episode_count 100
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v1234' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # -p iteration_count 20 -p eval_episode_count 10
-
-### Rinse and repeat
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v12' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # -p iteration_count 20 -p eval_episode_count 10
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v123' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # -p iteration_count 20 -p eval_episode_count 100
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v1234' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # -p iteration_count 20 -p eval_episode_count 10
+for i in {1..3}; do
+  for reward in "-p reward_clip True" ""; do
+    for gamma in "-p gamma 0.25" ""; do
+        cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v12' -p training_episode_count  2000 $reward $gamma # -p iteration_count 20 -p eval_episode_count 10
+        cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v123' -p training_episode_count  2000 $reward $gamma # -p iteration_count 20 -p eval_episode_count 100
+        cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_gradual_increase.ipynb -p gymid 'CyberBattleTinyMicro-v1234' -p training_episode_count  2000 $reward $gamma # -p iteration_count 20 -p eval_episode_count 10
+    done
+  done
+done
--- run_exper_ht_one_by_one.sh
+++ run_exper_ht_one_by_one.sh
@@ -9,22 +9,16 @@ OUTPUT_DIR=notebooks  #../../../../
 INPUT_DIR=cyberbattle/agents/baseline/notebooks
 REWARD_CLIP=True
 
-# cat $INPUT_DIR/notebook_debug_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v1' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
+# cat $INPUT_DIR/notebook_debug_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v1' -p training_episode_count  2000  $arg # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
 
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v1' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
 
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v2' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v3' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v4' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
-
-### Rinse and repeat
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v1' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v2' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v3' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
-
-cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v4' -p training_episode_count  2000  -p reward_clip $REWARD_CLIP # --log-output --log-level INFO --progress-bar  -p iteration_count 20 -p eval_episode_count 10
+for i in {1..3}; do
+  for reward in "-p reward_clip True" ""; do
+    for gamma in "-p gamma 0.25" ""; do
+        cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v1' -p training_episode_count 2000 $reward $gamma
+        cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v2' -p training_episode_count 2000 $reward $gamma
+        cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v3' -p training_episode_count 2000 $reward $gamma
+        cat $INPUT_DIR/notebook_dql_debug_with_tinymicro.py | jupytext --to ipynb --set-kernel - | papermill $OUTPUT_DIR/notebook_dql_debug_tiny_ht_one_by_one.ipynb -p gymid 'CyberBattleTinyMicro-v4' -p training_episode_count 2000 $reward $gamma
+    done
+  done
+done
